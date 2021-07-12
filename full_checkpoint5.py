298a1695516a7dc45917650b182dfddab29ae5aa
yassine2403 <86617346+yassine2403@users.noreply.github.com>
2021-07-12 17:53:30 +0100

    Update full_checkpoint5.py

--- full_checkpoint5.py
+++ full_checkpoint5.py
@@ -1,14 +1,16 @@
+from math import pi
 ##this is question 1
 class Print3D:
     def __init__(self,x,y,z):
         self.x=x
         self.y=y
         self.z=z
-        xyz=[x,y,z]
-        print(xyz)
+    def coord(self):
+        xyz=(self.x,self.y,self.z)
+        return xyz
 
 a=Print3D(3,4,5)
-
+print(a.coord())
 ##this is question 2
 class rectangle:
     def __init__(self,length,width):
@@ -16,14 +18,14 @@ class rectangle:
         self.width=width
     def area(self):
         self.aire=self.length*self.width
-        print(self.aire)
+        return (self.aire)
     def perimeter(self):
         self.perimeter=(self.length+self.width)*2
-        print(self.perimeter)
+        return (self.perimeter)
 
 rectangle=rectangle(2,3)
-rectangle.area()
-rectangle.perimeter()
+print('area of rectangle is approximately = to ',rectangle.area(),'and','perimeter of rectangle is approximatly = to ',rectangle.perimeter())
+
 
 ##this is question 3       
 class circle:
@@ -32,10 +34,10 @@ class circle:
         self.r=r
     def area(self):
         self.area=(self.r**2)*pi
-        print(self.area)
+        return (self.area)
     def perimeter(self):
         self.perimeter=self.r*2*pi
-        print(self.perimeter)
+        return (self.perimeter)
     def isinside(self,point_coord):
         a=self.a=self.xycoord_center[0]
         b=self.b=self.xycoord_center[1]
@@ -43,14 +45,15 @@ class circle:
         y=point_coord[1]
         distance_center_point=((x-a)**2+(y-b)**2)**0.5
         if distance_center_point<self.r:
-            print('the point with your ccordinates is inside the circle')
+            return('the point with your ccordinates is inside the circle')
         elif distance_center_point==self.r:
-            print('the point with your ccordinates is on the circumference of the circle')
+            return('the point with your ccordinates is on the circumference of the circle')
         else:
-            print('the point with your ccordinates is outside of the circle')
+            return('the point with your ccordinates is outside of the circle')
 
 circle=circle([0,0],4)
-circle.isinside([3,0])
+print('area of circle is approximately = to ',circle.area(),'and','perimeter of circle is approximatly = to ',circle.perimeter())
+print(circle.isinside([3,0]))
 
 
 ##this is question 3 
@@ -60,17 +63,10 @@ class Bank_account:
     def deposit(self,deposit):
         self.deposit=deposit
         self.account_money=self.account_money+self.deposit
-        print(self.account_money)
+        return(self.account_money)
     def withdraw(self,withdraw):
         self.withdraw=withdraw
         self.account_money=self.account_money-self.withdraw
-        print(self.account_money)
+        return(self.account_money)
         
-     
-Bank_account=Bank_account(6000)
-Bank_account.deposit(500)
 
-
-
-
-        

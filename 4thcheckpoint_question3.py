e5084086ec0f5c419d4fe924abd7b43b55d1e811
yassine2403 <86617346+yassine2403@users.noreply.github.com>
2021-07-12 14:26:19 +0100

    Update 4thcheckpoint_question3.py

--- 4thcheckpoint_question3.py
+++ 4thcheckpoint_question3.py
@@ -6,27 +6,26 @@ import numpy as np
 ##    for i in range (int(a)):
 ##        liste.append(int(input("enter a number")))
 ##        print(liste)
-##    print(sum(liste))
+##    return(sum(liste))
 
 ##def func2():
 ##    for i in range (int(a)):
 ##        liste.append(int(input("enter a number")))
 ##        print(liste)
-##    print(np.prod(liste))
+##    return(np.prod(liste))
 '''question3_3'''
 ##listedebase=[2,4,6,17]
 ##listeeven=[]
 ##listeodd=[]
 ##def Multiply(L):
-##    print(np.prod(L))
+##    return(np.prod(L))
 ##def Sum(L):
-##    print(sum(L))
+##    return(sum(L))
 ##
 ##for i in range (len(listedebase)):
 ##    if i%2==0:
 ##        listeeven.append(listedebase[i])
 ##    else:
 ##        listeodd.append(listedebase[i])
-##Multiply(listeodd)
-##Sum(listeeven)
+
 

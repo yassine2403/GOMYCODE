e348b5ca27b9db3c3d4fc4e1325b897a00e28879
yassine2403 <86617346+yassine2403@users.noreply.github.com>
2021-07-12 14:22:11 +0100

    Update 4thcheckpoint_question1.py

--- 4thcheckpoint_question1.py
+++ 4thcheckpoint_question1.py
@@ -1,12 +1,12 @@
 a=input("enter first number")
 b=input("enter second number")
 c=input("enter third number")
-def abc_check ():
+def abc_check (a,b,c):
     if a>b and a>c:
         print(a,"is the biggest")
     elif b>a and b>c:
         print(b,"is the biggest")
     else:
         print(c,"is the biggest")
-abc_check()
+abc_check(a,b,c)
     

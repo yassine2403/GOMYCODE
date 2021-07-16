6063ae2124aaff06facdc82528a2214b99a95d55
yassine2403 <86617346+yassine2403@users.noreply.github.com>
2021-07-16 15:10:34 +0100

    Update checkpoint8.py

--- checkpoint8.py
+++ checkpoint8.py
@@ -1,23 +1,33 @@
 f =open("texte.txt",'r',encoding = 'utf-8')
-for l in f:
-    print(l)
+
 def nth_line_reader(f,n):
     for i in range (n):
-        return f.readline()
+        print(f.readline())
+        
+n=input('enter number of lines you want to print counting from the top')
+print('the first ' +n+' lines are')
+nth_line_reader(f,int(n))
 
 def lastnth_lines(f,n):
+    str1=''
     lines=f.readlines()
     last_lines = lines[-n-1:]
     for i in last_lines:
-        a=i.replace('n/','')
-        return a
-def nb_lines(f):
+        str1=str1+i
+    return(str1)
+
+t=input('enter number of lines you want to print counting from the bottom')
+print('the last ' +t+' lines are')
+print(lastnth_lines(f,int(t)))
+
+def nb_words(f):
     b=[]
     a=0
     for i in f:
         b=b+i.split()
     for i in b:
         a=a+1
-    return a
+    return(a)
 
+print(nb_words(f))
     
